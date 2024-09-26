0e14a59b062289de148a6a4eb167ba22d1fe6f2b
Shameem Ahmad <shameem.ahmad7714@gmail.com>
2024-09-26 22:11:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
-function toCase(text) {
+function toCase(str) {
   // write your code here
+	  // Check if the input string is empty
+    if (str === '') {
+        return '-';
+    }
+
+    // Convert the string to lowercase and uppercase
+    const lowerCaseStr = str.toLowerCase();
+    const upperCaseStr = str.toUpperCase();
+
+    // Return the formatted string with '-' as the delimiter
+    return `${lowerCaseStr}-${upperCaseStr}`;
 }
 
 // DO not change the code below
